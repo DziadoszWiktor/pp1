12ceb6f7ed1536ab9a557a9aa99ad537dc208071
s-115-10 <s-115-10@gumiforest.ae.krakow.pl>
2019-11-18 13:02:10 +0100

    cw 05

---
+++ 05-ModularProgramming/c4.py
@@ -0,0 +1,21 @@
+import math
+x = 3.7
+y = 4
+a = 1/y
+
+sqrtx = math.sqrt(x)
+print(f'Pierwiastek kwadratowy x {x} wynosi {sqrtx}')
+
+powx = math.pow(x,y)
+print(f'Wynik potegi jest {powx}')
+
+powxy = math.pow(x,a)
+print(f'Pierwiatsek y-tego stopnia z x to {powxy}')
+
+pole = (math.pow(y,2)*math.pi)
+print(f'Pole kola to: {pole}')
+
+silnia = (math.factorial(y))
+print(f'Silnia y to: {silnia}')
+
+
